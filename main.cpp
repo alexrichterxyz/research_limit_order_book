61ab44cb4345fd9aeeec98890e3d821b98e32a51
Alexander Richter <8991alexander@gmail.com>
2023-01-20 00:53:03 +1100

    Moved headers into /src/include

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
-#include "book.hpp"
-#include "stop.hpp"
-#include "trailing_stop.hpp"
+#include "src/include/book.hpp"
+#include "src/include/stop.hpp"
+#include "src/include/trailing_stop.hpp"
 #include <iostream>
 #include <memory>
 
-
 int main(int argc, char *argv[]) {
 	auto my_book = elob::book();
-
-
 
 	return 0;
 }
